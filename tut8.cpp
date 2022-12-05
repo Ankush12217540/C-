e4405d4c27a33af82a05ab8f30f1c7a80804b037
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-05 23:55:19 +0530

    Setw,operator precedence and constant in C++

--- tut8.cpp
+++ tut8.cpp
@@ -10,8 +10,8 @@ int main(){
 // ***CONSTANT IN C++
 // const int a=3;
 // cout<<"value of a is: "<<a;
- //a=45;  //you will get an error because a is a constant
- //cout<<"The value of a is: "<<a<<endl;
+//  a=45;  //you will get an error because a is a constant
+//  cout<<"The value of a is: "<<a<<endl;
 //  int a=3, b=78, c=1233;
 //  cout<<"The value of a without setw is: "<<a<<endl;
 //  cout<<"The value of b without setw is: "<<b<<endl;
@@ -22,8 +22,8 @@ int main(){
 //  cout<<"The value of c is: "<<setw(4)<<c<<endl;
 // ***OPERATOR PRECEDENCE
 int a=3,b=4;
-//int c=(a*5)+b
-int c=((((a*5)+b)-45)+87);
+int c=(a*5)+b; 
+// int c=((((a*5)+b)-45)+87);
 cout<<c;
 return 0;
 }
