76f716586e11cb68b20e33fcfd03b0471214b77c
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-05 23:56:38 +0530

    if else , switch case statement in C++

--- tut9.cpp
+++ tut9.cpp
@@ -6,38 +6,38 @@ int main(){
     int age;
     cout<<"Tell me your age "<<endl;
     cin>>age;
-    // if((age<18) && (age>0)){
-    //     cout<<"you cannot come to my party"<<endl;
-    // }
-    // else if(age==18){
-    //     cout<<"you are a kid and you will get a kid pass to party"<<endl;
-    // }
-    // else if(age<1){
-    //     cout<<"you are not yet born"<<endl;
-    // }
-    // else{
-    //     cout<<"you can come to party"<<endl;
-    // }
+    if((age<18) && (age>0)){
+        cout<<"you cannot come to my party"<<endl;
+    }
+    else if(age==18){
+        cout<<"you are a kid and you will get a kid pass to party"<<endl;
+    }
+    else if(age<1){
+        cout<<"you are not yet born"<<endl;
+    }
+    else{
+        cout<<"you can come to party"<<endl;
+    }
 
 // selection control structure: Switch Case Statements
-switch(age)
-{
-    case 18:
-    cout<<"you are 18"<<endl;
-    break;
+// switch(age)
+// {
+//     case 18:
+//     cout<<"you are 18"<<endl;
+//     break;
 
-    case 2:
-    cout<<"you are 18"<<endl;
-    break;
+//     case 2:
+//     cout<<"you are 18"<<endl;
+//     break;
 
-    case 22:
-    cout<<"you are 22"<<endl;
-    break;
+//     case 22:
+//     cout<<"you are 22"<<endl;
+//     break;
 
-    default:
-    cout<<"no special cases"<<endl;
-      break;
-}
+//     default:
+//     cout<<"no special cases"<<endl;
+//       break;
+// }
      
     return 0;
 }
