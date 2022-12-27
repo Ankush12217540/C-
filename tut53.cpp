ecfca6d39ea6053b2bb0f04b2a9c05889fe13f3a
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-27 23:44:05 +0530

    Invoking Member Function

--- tut53.cpp
+++ tut53.cpp
@@ -13,7 +13,7 @@ class A{
     }
 };
 int main(){
-    // this is a keyword which is a ponter which points to the objects which which invokes the member function
+    // this is a keyword which is a pointer which points to the objects which which invokes the member function
     A a;
     a.setData(4).GetData();
     // a.GetData();  //--> isko explictly lagane ki jaroorat nahi hain,upar wale se ho jaayega.. using this function
