500512389c267a7560be087aab7df988656937ae
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-05-23 23:45:18 +0530

    tut12.cpp

--- tut12.cpp
+++ tut12.cpp
@@ -11,13 +11,13 @@ int main(){.
     // &------>(address of )operator
     cout<<"The address of a is "<<&a<<endl;
     cout<<"The address of a is "<<b<<endl; 
-   
-    //  *----->(value at) Dereference operator
-    cout<<"The value at address b is "<<&b<<endl;
+    
+    //  *----->(value at) Dereference operator 
+    cout<<"The value at address b is "<<&b<<endl; 
 
     // pointer to pointer
     int** c=&b;
-    cout<<"The address of b is "<<&b<<endl;
+    cout<<"The address  of b is "<<&b<<endl;
     cout<<"The address of b is "<<c<<endl;
     cout<<"The value at address c is "<<*c<<endl;
     cout<<"The value at address value at(value at (c)) is "<<**c<<endl;
